e8544c6ccb67fc28573ec6fc46332f1130c687f0
genkai1902 <namnh1902@gmail.com>
2021-05-13 21:12:35 +0700

    submit assignment 1

---
+++ SQL/Assignment 1/Testing_System_Assignment_1.sql
@@ -0,0 +1,73 @@
+DROP DATABASE IF EXISTS Testing_System_Assignment_1;
+CREATE DATABASE Testing_System_Assignment_1;
+
+USE Testing_System_Assignment_1;
+
+CREATE TABLE Department (
+	DepartmentID INT,
+    DepartmentName VARCHAR(50)
+    );
+
+CREATE TABLE `Position` (
+	PositionID INT,
+    PositionName VARCHAR(50)
+);
+
+CREATE TABLE `Account` (
+	AccountID INT,
+    Email VARCHAR(100),
+    Username VARCHAR(50),
+    FullName VARCHAR(50),
+    DepartmentID INT,
+    PositionID INT,
+    CreateDate DATE
+); 
+   
+CREATE TABLE `Group` (
+	GroupID INT,
+    GroupName VARCHAR(50),
+    CreatorID INT,
+    CreateDate DATE
+);
+
+CREATE TABLE GroupAccount(
+	GroupID INT,
+    AccountID INT,
+    JoinDate DATE
+);
+
+CREATE TABLE TypeQuestion(
+	TypeID INT,
+    TypeName VARCHAR(50)
+);
+
+CREATE TABLE Question(
+	QuestionID INT,
+    Content VARCHAR(50),
+    CategoryID INT,
+    TypeID INT,
+    CreatorID INT,
+    CreateDate DATE
+);
+
+CREATE TABLE Answer(
+	AnswerID INT,
+    Content VARCHAR(50),
+    QuestionID INT,
+    isCorrect BOOLEAN
+);
+
+CREATE TABLE Exam(
+	ExamID INT,
+    `Code` CHAR(5),
+    Title VARCHAR(50),
+    CategoryID INT,
+    Duration TIME,
+    CreatorID INT,
+    CreateDate DATE
+);
+
+CREATE TABLE ExamQuestion(
+	ExamID INT,
+    QuestionID INT
+);
