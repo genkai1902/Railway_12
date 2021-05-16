3e8f0089e3430def3d0168400c2e3dcfd5144b29
genkai1902 <namnh1902@gmail.com>
2021-05-16 09:38:57 +0700

    Update assignment 2

--- SQL/Assignment 2/Testing_System_Assignment_2.sql
+++ SQL/Assignment 2/Testing_System_Assignment_2.sql
@@ -4,75 +4,75 @@ CREATE DATABASE Testing_System_Assignment_2;
 USE Testing_System_Assignment_2;
 
 CREATE TABLE Department (
-	DepartmentID TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
-    DepartmentName VARCHAR(50) NOT NULL
-    );
+    DepartmentID 	MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
+    DepartmentName 	NVARCHAR(50) NOT NULL
+);
 
 CREATE TABLE `Position` (
-	PositionID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
-    PositionName ENUM("Dev","Test","Scrum Master", "PM") DEFAULT("Dev")
+	PositionID 		MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
+    PositionName 	ENUM("Dev","Test","Scrum Master", "PM") DEFAULT("Dev")
 );
 
 CREATE TABLE `Account` (
-	AccountID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
-    Email VARCHAR(100) NOT NULL,
-    Username VARCHAR(50) NOT NULL,
-    FullName VARCHAR(50) NOT NULL,
-    DepartmentID TINYINT REFERENCES Department(DepartmentID),
-    PositionID INT UNSIGNED REFERENCES `Position`(PositionID),
-    CreateDate DATE
+	AccountID 		MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
+    Email 			VARCHAR(100) NOT NULL,
+    Username 		NVARCHAR(50) NOT NULL,
+    FullName 		NVARCHAR(50) NOT NULL,
+    DepartmentID 	MEDIUMINT UNSIGNED REFERENCES Department(DepartmentID),
+    PositionID 		MEDIUMINT UNSIGNED REFERENCES `Position`(PositionID),
+    CreateDate 		DATETIME
 ); 
    
 CREATE TABLE `Group` (
-	GroupID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
-    GroupName VARCHAR(50) NOT NULL,
-    CreatorID INT REFERENCES `Account`(AccountID),
-    CreateDate DATE
+	GroupID 		MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
+    GroupName 		VARCHAR(50) NOT NULL,
+    CreatorID 		MEDIUMINT REFERENCES `Account`(AccountID),
+    CreateDate 		DATETIME
 );
 
 CREATE TABLE GroupAccount(
-	GroupID INT REFERENCES `Group`(GroupID),
-    AccountID INT UNSIGNED REFERENCES `Account`(AccountID),
-    JoinDate DATE
+	GroupID 		MEDIUMINT REFERENCES `Group`(GroupID),
+    AccountID 		MEDIUMINT UNSIGNED REFERENCES `Account`(AccountID),
+    JoinDate		DATETIME
 );
 
 CREATE TABLE TypeQuestion(
-	TypeID TINYINT AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
-    TypeName ENUM("Essay", "Multiple-Choice")
+	TypeID 			MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
+    TypeName 		ENUM("Essay", "Multiple-Choice")
 );
 
 CREATE TABLE CategoryQuestion (
-	CategoryID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    CategoryName VARCHAR(50)
+	CategoryID 		MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    CategoryName 	NVARCHAR(50)
 );
 
 CREATE TABLE Question(
-	QuestionID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Content VARCHAR(250),
-    CategoryID INT UNSIGNED REFERENCES CategoryQuestion(CategoryID),
-    TypeID TINYINT REFERENCES TypeQuestion(TypeID),
-    CreatorID INT REFERENCES `Account`(AccountID),
-    CreateDate DATE
+	QuestionID 		MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Content 		NVARCHAR(250),
+    CategoryID 		MEDIUMINT UNSIGNED REFERENCES CategoryQuestion(CategoryID),
+    TypeID 			MEDIUMINT UNSIGNED REFERENCES TypeQuestion(TypeID),
+    CreatorID 		MEDIUMINT UNSIGNED REFERENCES `Account`(AccountID),
+    CreateDate 		DATETIME
 );
 
 CREATE TABLE Answer(
-	AnswerID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Content VARCHAR(250),
-    QuestionID INT UNSIGNED REFERENCES Question(QuestionID),
-    isCorrect BOOLEAN NOT NULL
+	AnswerID 		MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Content 		NVARCHAR(250),
+    QuestionID 		MEDIUMINT UNSIGNED REFERENCES Question(QuestionID),
+    isCorrect 		BIT
 );
 
 CREATE TABLE Exam(
-	ExamID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `Code` CHAR(5) UNIQUE KEY,
-    Title VARCHAR(50),
-    CategoryID INT UNSIGNED REFERENCES CategoryQuestion(CategoryID),
-    Duration TINYINT,
-    CreatorID INT REFERENCES `Account`(AccountID),
-    CreateDate DATE
+	ExamID 			MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `Code` 			CHAR(5) UNIQUE KEY,
+    Title 			NVARCHAR(50),
+    CategoryID 		MEDIUMINT UNSIGNED REFERENCES CategoryQuestion(CategoryID),
+    Duration 		TINYINT,
+    CreatorID 		MEDIUMINT UNSIGNED REFERENCES `Account`(AccountID),
+    CreateDate 		DATETIME
 );
 
 CREATE TABLE ExamQuestion(
-	ExamID INT UNSIGNED REFERENCES Exam(ExamID),
-    QuestionID INT UNSIGNED REFERENCES Question.QuestionID
+	ExamID 			MEDIUMINT UNSIGNED REFERENCES Exam(ExamID),
+    QuestionID 		MEDIUMINT UNSIGNED REFERENCES Question.QuestionID
 );
