699fc2aabd07be2a673b5754954f8f7aad3a55f2
genkai1902 <namnh1902@gmail.com>
2021-05-18 04:01:22 +0700

    Add assignment 3

---
+++ SQL/Assignment 3/Testing_System_Assignment_3.sql
@@ -0,0 +1,83 @@
+USE TestingSystem3;
+
+-- Question 2: lấy ra tất cả các phòng ban
+SELECT 		*
+FROM		Department
+ORDER BY	DepartmentID;
+
+-- Question 3: lấy ra id của phòng ban "Sale"
+SELECT		*
+FROM		Department
+WHERE		DepartmentName = 'Sale';
+
+-- Question 4: lấy ra thông tin account có full name dài nhất
+SELECT		*
+FROM		Account
+ORDER BY 	length(FullName) DESC
+LIMIT 		1;
+		
+
+-- Question 5: Lấy ra thông tin account có full name dài nhất và thuộc phòng ban có id = 3
+SELECT		*
+FROM		Account
+WHERE		DepartmentID = 3
+ORDER BY	length(FullName) DESC
+LIMIT		1;
+
+-- Question 6: Lấy ra tên group đã tham gia trước ngày 20/12/2019
+SELECT		*
+FROM		`Group`
+WHERE		CreateDate < '2019-12-20';
+
+-- Question 7: Lấy ra ID của question có >= 4 câu trả lời
+SELECT		QuestionID, Count(QuestionID) AS Number_of_Answer
+FROM		Answer
+GROUP BY	QuestionID
+HAVING		COUNT(QuestionID) >= 4;
+
+-- Question 8: Lấy ra các mã đề thi có thời gian thi >= 60 phút và được tạo trước ngày 20/12/2019
+SELECT		ExamID, Title
+FROM		Exam
+WHERE		Duration >= 60 AND CreateDate < '2019-12-20';
+
+-- Question 9: Lấy ra 5 group được tạo gần đây nhất
+SELECT		GroupName, CreateDate
+FROM		`Group`
+ORDER BY	CreateDate DESC
+LIMIT		5;
+
+-- Question 10: Đếm số nhân viên thuộc department có id = 2
+SELECT		COUNT(*) AS 'So nhan vien', DepartmentID
+FROM		`Account`
+WHERE		DepartmentID = 2;
+
+-- Question 11: Lấy ra nhân viên có tên bắt đầu bằng chữ "T" và kết thúc bằng chữ "e"
+SELECT		*
+FROM		`Account`
+WHERE		FullName LIKE 'T%e';
+
+-- Question 12: Xóa tất cả các exam được tạo trước ngày 20/12/2019
+DELETE
+FROM		Exam
+WHERE		CreateDate < '2019-12-20';
+
+-- Question 13: Xóa tất cả các question có nội dung liên quan "%3642%"
+SELECT		*
+FROM		Question;
+
+DELETE
+FROM		Question
+WHERE		Content LIKE '%3642%';
+
+-- Question 14: Update thông tin của account có id = 5 thành tên "Nguyễn Bá Lộc" và email thành loc.nguyenba@vti.com.vn
+UPDATE		`Account`
+SET			FullName = 'Nguyễn Bá Lộc', Email = 'loc.nguyenba@vti.com.vn'
+WHERE		AccountID = 5;
+
+-- Question 15: update account có id = 5 sẽ thuộc group có id = 4
+SELECT		*
+FROM		GroupAccount;
+
+UPDATE		`GroupAccount`
+SET			GroupID = 2
+WHERE		AccountID = 13;
