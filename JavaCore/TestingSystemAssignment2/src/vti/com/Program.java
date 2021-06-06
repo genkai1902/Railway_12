4c2f4e1b55659ab0f329b14aa05c010663be021b
genkai1902 <namnh1902@gmail.com>
2021-06-06 10:18:03 +0700

    Modify

--- JavaCore/TestingSystemAssignment2/src/vti/com/Program.java
+++ JavaCore/TestingSystemAssignment2/src/vti/com/Program.java
@@ -3,5 +3,6 @@ package vti.com;
 public class Program {
     public static void main (String[] args){
         System.out.println("Hello World!");
+        System.out.println("Day la github cua toi");
     }
 }
